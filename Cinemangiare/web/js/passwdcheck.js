b5574361043c0a0bff00d9dbfd37e1f55cafe026
leouzz <leonanig@gmail.com>
2015-08-21 18:29:01 +0200

    Io lavoro, e voi?

---
+++ Cinemangiare/web/js/passwdcheck.js
@@ -0,0 +1,28 @@
+function contpass()
+{
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('passwd');
+    var pass2 = document.getElementById('passwd2');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match.
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "OK!"
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Le Password non combaciano!"
+    }
+}
